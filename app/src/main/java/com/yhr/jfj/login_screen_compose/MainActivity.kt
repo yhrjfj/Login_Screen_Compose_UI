494f34d9589ac0539cbedf8bd3172bd8369ed3cf
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-16 18:09:19 +0600

    Initial commit

---
+++ app/src/main/java/com/yhr/jfj/login_screen_compose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.yhr.jfj.login_screen_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yhr.jfj.login_screen_compose.ui.theme.Login_Screen_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Login_Screen_ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Login_Screen_ComposeTheme {
+        Greeting("Android")
+    }
+}
