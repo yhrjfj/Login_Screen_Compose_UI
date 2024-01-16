267494cc2e58992a94ce6ae994552bfd1508e165
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-16 19:39:57 +0600

    Add LoginScreen in MainActivity

--- app/src/main/java/com/yhr/jfj/login_screen_compose/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/login_screen_compose/MainActivity.kt
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Login_Screen_ComposeTheme {
-        Greeting("Android")
     }
 }
