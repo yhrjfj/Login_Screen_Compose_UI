80b3164dde4a12b42aa15302a30deb595398ad1b
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-16 20:28:34 +0600

    Add some element in top section

--- app/src/main/java/com/yhr/jfj/login_screen_compose/LoginScreen.kt
+++ app/src/main/java/com/yhr/jfj/login_screen_compose/LoginScreen.kt
@@ -4,16 +4,26 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.yhr.jfj.login_screen_compose.ui.theme.Black
 
 @Composable
@@ -34,6 +44,30 @@ fun LoginScreen() {
                         .fillMaxHeight(fraction = 0.46f),
                     contentScale = ContentScale.FillBounds
                 )
+                Row(
+                    modifier = Modifier.padding(top = 80.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = stringResource(id = R.string.app_logo),
+                        modifier = Modifier.size(42.dp),
+                        tint = uiColor
+                    )
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Column {
+                        Text(
+                            text = stringResource(id = R.string.the_tolet),
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = uiColor
+                        )
+                        Text(
+                            text = stringResource(id = R.string.find_house),
+                            style = MaterialTheme.typography.titleMedium,
+                            color = uiColor
+                        )
+                    }
+                }
             }
         }
     }
