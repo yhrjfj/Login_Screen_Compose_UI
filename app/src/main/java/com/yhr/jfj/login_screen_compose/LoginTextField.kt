c84e30a22c6aa49c817274c1d3fb7b5961215ae3
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-16 20:55:24 +0600

    Create LoginTextField component

---
+++ app/src/main/java/com/yhr/jfj/login_screen_compose/LoginTextField.kt
@@ -0,0 +1,52 @@
+package com.yhr.jfj.login_screen_compose
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.yhr.jfj.login_screen_compose.ui.theme.Black
+import com.yhr.jfj.login_screen_compose.ui.theme.focusedTextFieldText
+import com.yhr.jfj.login_screen_compose.ui.theme.textFieldContainer
+import com.yhr.jfj.login_screen_compose.ui.theme.unfocusedTextFieldText
+
+@Composable
+fun LoginTextField(
+    modifier: Modifier = Modifier,
+    label: String,
+    trailing: String
+) {
+    val uiColor = if (isSystemInDarkTheme()) Color.White else Black
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = modifier,
+        label = {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.labelMedium,
+                color = uiColor
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedPlaceholderColor = MaterialTheme.colorScheme.unfocusedTextFieldText,
+            focusedPlaceholderColor = MaterialTheme.colorScheme.focusedTextFieldText,
+            unfocusedContainerColor = MaterialTheme.colorScheme.textFieldContainer,
+            focusedContainerColor = MaterialTheme.colorScheme.textFieldContainer
+        ),
+        trailingIcon = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(
+                    text = trailing,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Medium),
+                    color = uiColor
+                )
+            }
+        }
+    )
+}
