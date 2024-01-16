b349a115090914ac40d5bc606e434072db3c80fe
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-16 21:30:47 +0600

    Create SocialMediaLogin

--- app/src/main/java/com/yhr/jfj/login_screen_compose/SocialMediaLogin.kt
+++ app/src/main/java/com/yhr/jfj/login_screen_compose/SocialMediaLogin.kt
@@ -3,15 +3,24 @@ package com.yhr.jfj.login_screen_compose
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScopeMarker
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.yhr.jfj.login_screen_compose.ui.theme.BlueGray
 import com.yhr.jfj.login_screen_compose.ui.theme.LightBlueWhite
@@ -26,8 +35,22 @@ fun SocialMediaLogin(
     Row(
         modifier = modifier
             .clip(RoundedCornerShape(4.dp))
+            .socialMedia()
+            .clickable { onClick() }
+            .height(40.dp)
     ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp)
+        )
 
+        Spacer(modifier = Modifier.width(5.dp))
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(color = Color(0xFF64748B))
+        )
     }
 }
 
