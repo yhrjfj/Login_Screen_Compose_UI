c09a2a951b7b71844e2af66ea0643aa3630e9cea
RiJo <102893295+yhrjfj@users.noreply.github.com>
2024-01-16 21:58:45 +0600

    Do alignment in SocialMediaLogin component

--- app/src/main/java/com/yhr/jfj/login_screen_compose/SocialMediaLogin.kt
+++ app/src/main/java/com/yhr/jfj/login_screen_compose/SocialMediaLogin.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -15,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
@@ -37,7 +39,9 @@ fun SocialMediaLogin(
             .clip(RoundedCornerShape(4.dp))
             .socialMedia()
             .clickable { onClick() }
-            .height(40.dp)
+            .height(40.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             painter = painterResource(id = icon),
